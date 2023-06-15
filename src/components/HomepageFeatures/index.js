ff4f2f7dc7476571e464a183775695d635ee2614
zhushen <60245376+zhushen12580@users.noreply.github.com>
2023-06-15 09:31:52 +0800

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -8,7 +8,7 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        欧美流行硅胶母婴用品，跨境电商热销橱柜垫、餐垫、餐具、厨具、烘焙用具批发。
+        欧美流行硅胶家居用品，跨境电商热销橱柜垫、餐垫、餐具、厨具、烘焙用具批发。
       </>
     ),
   },
@@ -17,12 +17,12 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        欧美流行硅胶母婴用品，跨境电商热销宠物垫、狗碗、狗粮袋、宠物玩具、宠物清洁用具批发。
+        欧美流行硅胶宠物用品，跨境电商热销宠物垫、狗碗、狗粮袋、宠物玩具、宠物清洁用具批发。
       </>
     ),
   },
   {
-    title: '婴儿用品',
+    title: '母婴用品',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
