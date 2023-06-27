469d7b752b2bd0c56a2584b6335282d90b55f011
zhushen <2270364052@qq.com>
2023-06-27 16:17:19 +0800

    更新域名
    更新域名

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/文锋logo.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://hrmtechsz.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
